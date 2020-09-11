181d47ad5c9aac824ef541aa4876b64b06fca636
JenVest2020 <jenvest2019@gmail.com>
2020-09-11 16:37:52 -0500

    showsRouter completed

---
+++ data/shows/showsRouter.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const showsDb = require('../helpers/showsModel');
+
+const router = express.Router();
+
+//Create
+router.post('/', (req, res) => {
+    const info = req.body;
+    showsDb.insert(info)
+        .then(info => {
+            res.status(201).json(info);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Error saving show to database' });
+        });
+});
+//Read
+router.get('/', (req, res) => {
+    showsDb.get(req.id)
+        .then(e => {
+            res.status(200).json(e);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Error in retrieving show!' });
+        });
+});
+
+//Update
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    const id = req.params.id;
+    showsDb.update(id, changes)
+        .then(changes => {
+            if (changes) {
+                res.status(200).json(changes);
+            } else {
+                res.status(404).json({ message: 'Show not found' });
+            }
+
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'error: database could not be modified' });
+        });
+});
+
+//Delete
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    showsDb.remove(id)
+        .then(e => {
+            if (e) {
+                res.status(200).json({ message: 'The show has been deleted' });
+            } else {
+                res.status(404).json({ message: 'Show not found' });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Show could not be removed from database' });
+        });
+});
+
+//getCharacters
+router.get('/:id/characters', (req, res) => {
+    showsDb.getShowsCharacters(req.params.id)
+        .then(e => {
+            res.status(200).json(e);
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'Could not retieve info from database' });
+        });
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const server = express();
+const showsRouter = require('./data/shows/showsRouter.js');
 
 server.use(express.json());
 server.use(helmet());
@@ -8,5 +9,7 @@ server.use(helmet());
 server.get('/', (req, res) => {
     res.status(200).json({ message: 'The server is online!' });
 });
+
+server.use('/api/shows', showsRouter);
 
 module.exports = server;
